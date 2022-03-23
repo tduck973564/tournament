92c0c3dc13fc79d5304eafcb2b6d21035b7bde7a
tduck973564 <tduck973564@gmail.com>
2022-03-23 17:27:41 +1100

    packaging scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "me.thoma"
-version = "1.0-SNAPSHOT"
+version = System.getenv("TOURNAMENT_VERSION")
 
 repositories {
     mavenCentral()
@@ -36,6 +36,23 @@ tasks.test {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+
+        archiveClassifier.set("standalone") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+}
+
 
 application {
     mainClass.set("MainKt")
